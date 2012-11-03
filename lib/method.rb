fca084eaf22528e25526449a48454816c1c5b4f8
Nolan Blew <nolanblew@hotmail.com>
2012-11-02 19:26:11 -0700

    Added sample gem templates and modified Gems

--- lib/method.rb
+++ lib/method.rb
@@ -1,6 +1,18 @@
 module Gmailer
 	def generate username, password
 	
+		if (username != "" && password != "")
+			#Check to see if there this already exists in enviornmnet.rb
+			File.new("file.rb", "w")
+			file = File.open("file.rb");
+			file.puts("module file");
+			file.puts("\t#Username: " + username)
+			file.puts("\t#Password: " + password)
+			file.puts("\nend")
+			file.close
+		else
+			puts "Error. Please provide a username and a password for your GMail account."
+		end
 	
 	end
 end
