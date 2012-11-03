a15192e8630160e5d686e259231e6f559020225f
Nolan Blew <nolanblew@hotmail.com>
2012-11-02 23:20:04 -0700

    fixed file issue

--- lib/gmailer.rb
+++ lib/gmailer.rb
@@ -2,14 +2,14 @@ class Gmailer
 	def self.generate username, password
 			
 		if !(username == "" || password == "")
-			File.new("tmp.txt", "w")
-			file = File.open("tmp.txt")
-			file.puts "username: " + username
-			file.puts "password: " + password + "\n"
+			file = File.new("tmp.txt", "w")
+			file.puts("username: " + username)
+			file.puts("password: " + password + "\n")
 			file.close()
 			puts "Generated temporary file."
 		else
 			puts "Error. Please put your gmail username and password."
 		end
+		
 	end
 end
